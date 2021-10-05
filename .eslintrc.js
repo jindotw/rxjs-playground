7a98610e18d551fa892372ac7c0f8e7b45d41ef4
paul.lin <paul.lin@cherry.tv>
2021-10-05 13:16:05 +0800

    chore: tweaked eslint to allow unused vars and empty function

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  rules: {
+    "no-unused-vars": "off",
+    "no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": 0,
+    "@typescript-eslint/no-empty-function": 0,
+  }
 };
